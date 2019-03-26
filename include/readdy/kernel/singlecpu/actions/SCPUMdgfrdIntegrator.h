5e2d92f2bff6e473b86250793bc1d9ca850f59a4
luigisbailo <luigi.sbailo@gmail.com>
2019-03-26 16:41:24 +0100

    main logical steps of mdgfrd action

--- include/readdy/kernel/singlecpu/actions/SCPUMdgfrdIntegrator.h
+++ include/readdy/kernel/singlecpu/actions/SCPUMdgfrdIntegrator.h
@@ -53,7 +53,7 @@ struct MdgfrdParticleData {
     scalar exitTime = 0;
     scalar constructionTime = 0;
     scalar domainSize = 0;
-
+    Vec3 exitPosition  = {0,0,0};
     bool GFpropagation = false;
 };
 
@@ -122,21 +122,115 @@ public:
         auto& stateModel = kernel->getSCPUKernelStateModel();
         const auto pd = stateModel.getParticleData();
         const auto t = stateModel.time();
-        for (auto& entry : *pd){
+        const auto &nl = stateModel.getNeighborList();
+
+        for (auto& entry : *pd) {
 
         }
 
+        // fractional propagation after exiting the protective domain
         for(auto& entry : *pd) {
             if(!entry.is_deactivated()) {
-                const scalar D = context.particleTypes().diffusionConstantOf(entry.type);
-                const auto randomDisplacement = std::sqrt(2. * D * _timeStep) *
-                                                (readdy::model::rnd::normal3<readdy::scalar>());
-                entry.pos += randomDisplacement;
-                const auto deterministicDisplacement = entry.force * _timeStep * D / kbt;
-                entry.pos += deterministicDisplacement;
-                bcs::fixPosition(entry.pos, box, pbc);
+                if (particleData.at(entry.id).GFpropagation && particleData.at(entry.id).exitTime < stateModel.time()){
+
+                    const scalar D = context.particleTypes().diffusionConstantOf(entry.type);
+                    const auto randomDisplacement = std::sqrt(2. * D * (stateModel.time()-particleData.at(entry.id).exitTime )) *
+                                                    (readdy::model::rnd::normal3<readdy::scalar>());
+                    entry.pos = particleData.at(entry.id).exitPosition + randomDisplacement;
+                    bcs::fixPosition(entry.pos, box, pbc);
+                    particleData.at(entry.id).GFpropagation = false;
+                    particleData.at(entry.id).domainSize = 0;
+
+                }
             }
         }
+
+        if (nl->cutoff() > 0) {
+            stateModel.updateNeighborList();
+        }
+
+        // bursts - iteration over cells, it might be more performant to iterate over particles
+        for (auto cell = 0_z; cell < nl->nCells(); ++cell) {
+            for (auto it = nl->particlesBegin(cell); it != nl->particlesEnd(cell); ++it) {
+                auto pidx = *it;
+                auto &&entry = pd->entry_at(pidx);
+                if (particleData.at(entry.id).GFpropagation ) {
+                    nl->forEachNeighbor(it, cell, [&](const std::size_t neighbor) {
+                        auto &&neighborEntry = pd->entry_at(neighbor);
+                        if (!particleData.at(neighborEntry.id).GFpropagation) {
+                            auto dij = bcs::dist(entry.pos, neighborEntry.pos, box, pbc);
+                            if (dij - maxInteractionRadius.at(std::make_pair(entry.type,neighborEntry.type)) <
+                            particleData.at(entry.id).domainSize + speciesData.at(neighborEntry.type).minDomainSize){
+                                //entry is burst
+                                // @todo - WRONG SAMPLING - the radius should be sample from appropriate PDF
+                                scalar radius = readdy::model::rnd::uniform_real() *  particleData.at(entry.id).domainSize;
+                                Vec3 displacement = randomOrientation(radius);
+                                entry.pos += displacement;
+                                bcs::fixPosition(entry.pos, box, pbc);
+                                particleData.at(entry.id).GFpropagation = false;
+                                particleData.at(entry.id).domainSize = 0;
+                                particleData.at(entry.id).exitTime = stateModel.time();
+
+                            }
+                        }
+                    });
+                }
+            }
+        }
+
+        if (nl->cutoff() > 0) {
+            stateModel.updateNeighborList();
+            // @todo calc forces
+        }
+
+        // propagate or construct domain - it might be more performant to iterate over particles
+        for (auto cell = 0_z; cell < nl->nCells(); ++cell) {
+            for (auto it = nl->particlesBegin(cell); it != nl->particlesEnd(cell); ++it) {
+                auto pidx = *it;
+                auto &&entry = pd->entry_at(pidx);
+                if (!particleData.at(entry.id).GFpropagation ) {
+                    scalar domainSize =  speciesData.at(entry.type).maxDomainSize;
+                    nl->forEachNeighbor(it, cell, [&](const std::size_t neighbor) {
+                        auto &&neighborEntry = pd->entry_at(neighbor);
+                        if (!particleData.at(neighborEntry.id).GFpropagation) {
+
+                            auto dij = bcs::dist(entry.pos, neighborEntry.pos, box, pbc);
+                            auto dijExit = bcs::dist(entry.pos, particleData.at(neighborEntry.id).exitPosition, box, pbc);
+                            //@todo implement algorithm for domain size
+                            scalar tempDomainSize = dij/2 - maxInteractionRadius.at(std::make_pair(entry.type,neighborEntry.type));
+
+                            if (tempDomainSize<domainSize){
+                                domainSize = tempDomainSize;
+                            }
+                        }
+                    });
+
+                    if (domainSize>speciesData.at(entry.type).minDomainSize){
+
+//                      @todo the exit-time should be sampled from an appropriate PDF
+                        scalar tau = 10 * timeStep();
+                        particleData.at(entry.id).GFpropagation = true;
+                        particleData.at(entry.id).domainSize = domainSize;
+                        particleData.at(entry.id).exitTime = stateModel.time() + tau;
+                        Vec3 displacement = randomOrientation(domainSize);
+                        particleData.at(entry.id).exitPosition, box, pbc) = entry.pos + displacement;
+                    }
+                    else{
+
+                        const scalar D = context.particleTypes().diffusionConstantOf(entry.type);
+                        const auto randomDisplacement = std::sqrt(2. * D * _timeStep) *
+                                                        (readdy::model::rnd::normal3<readdy::scalar>());
+                        entry.pos += randomDisplacement;
+                        //WARNING: forces not updated
+                        const auto deterministicDisplacement = entry.force * _timeStep * D / kbt;
+                        entry.pos += deterministicDisplacement;
+                        bcs::fixPosition(entry.pos, box, pbc);
+
+                    }
+                }
+            }
+        }
+
     }
 
 private:
@@ -151,6 +245,26 @@ private:
 
     std::map<readdy::model::Particle::id_type, MdgfrdParticleData> particleData;
     std::unordered_map<readdy::model::Particle::type_type, MdgfrdSpeciesData> speciesData;
+    util::particle_type_pair_unordered_map<scalar> maxInteractionRadius;
+
+    Vec3 randomOrientation ( scalar radius ) {
+
+        //Theta is defined in [0,2pi]
+        //Phi is defined in [0,pi]
+        auto u = readdy::model::rnd::uniform_real();
+        auto v = readdy::model::rnd::uniform_real();
+        // @todo check if there is internal pi definition
+        double theta = 2 * M_PI * u;
+        double phi = std::acos( 2*v - 1 );
+
+        Vec3 vector;
+
+        vector[0] = radius * cos(theta) * sin(phi);
+        vector[1] = radius * sin(theta) * sin(phi);
+        vector[2] = radius * cos(phi);
+
+        return vector;
+    }
 
 };
 
